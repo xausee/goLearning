0e0f5601ac8f3174babe0c28e5c994bcb8d5bf30
xausee <xausee@gmail.com>
2013-08-06 19:51:29 +0800

    update bunblesort functions

--- BubbleSort.go
+++ BubbleSort.go
@@ -2,43 +2,66 @@ package main
 
 import "fmt"
 
-func bubblesortone(m []int, flag bool) {     
-  for i := 0; i < len(m) - 1; i++ {    
-    for j := i + 1 ; j < len(m); j++ {
+/* from start to end
+   flag = true ascending order
+   flag = false descending order */
+func BubbleSortOne(m []int, flag bool) {  
+  b := false   
+  o: for i := 0; i < len(m) - 1; i++ {      
+    if b {
+      break o
+    }
+    b = true 
+    for j := i + 1; j < len(m); j++ {           
       if flag {
-        if m[i] > m[j] {
-          m[i], m[j] = m[j], m[i] 
+        if m[i] > m[j] {          
+          b, m[i], m[j] = false, m[j], m[i] 
         } 
       } else {
-          if m[i] < m[j] {
-          m[i], m[j] = m[j], m[i] 
+          if m[i] < m[j] {            
+            b, m[i], m[j] =false, m[j], m[i] 
           }
-      }       
-    }  
-  }  
+      }      
+    }
+    fmt.Println(i,m) 
+  }   
 }
 
-func bubblesorttwo(m []int, flag bool) {        
-  for i := len(m) - 1; i > 0; i-- {    
-    for j := 0; j < i; j++ {
+/* from end to start
+   flag = true ascending order
+   flag = false descending order */
+func BubbleSortTwo(m []int, flag bool) { 
+  b := false        
+  o: for i := 0; i < len(m); i++ {     
+    if b {
+      break o
+    }
+    b = true  
+    for j := 0; j < len(m)-i-1; j++ {
       if flag {
-        if m[j] > m[i] {
-         m[i], m[j] = m[j], m[i]
+        if m[j] > m[j+1] {
+          b, m[j+1], m[j] = false, m[j], m[j+1] 
         }
        } else {
-           if m[j] < m[i] {
-             m[i], m[j] = m[j], m[i]
+           if m[j] < m[j+1] {
+             b, m[j+1], m[j] =false, m[j], m[j+1] 
            }
-       }
-    }   
+       }           
+    } 
+    fmt.Println(i, m)      
   }  
 }
 
 func main() {
-  a := []int{1, 1000, 3, 4, 2, 5, 777, 43, 6, 23}
-  b := []int{1, 1000, 3, 4, 2, 5, 777, 43, 6, 23}
-  fmt.Println("unsorted array: ", a, b)
-  bubblesortone(a, false)
-  bubblesorttwo(b, true)
-  fmt.Println("sorted array: ", a, b) 
+  a := []int{45, 1, 1000, 3, 4, 2, 5, 777, 43, 6, 23} 
+  fmt.Println()
+  fmt.Println("Oringinal array:\n",a)
+  fmt.Println()
+  BubbleSortOne(a, true)
+  fmt.Println()
+  BubbleSortOne(a, false)
+  fmt.Println()
+  BubbleSortTwo(a, true)
+  fmt.Println()
+  BubbleSortTwo(a, false)  
 }

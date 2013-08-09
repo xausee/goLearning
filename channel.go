d8590ea223072e61025e68a33ce1aea703d478c9
xausee <xausee@gmail.com>
2013-08-09 18:07:25 +0800

    channel usage

---
+++ channel.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main(){
+	ch := make(chan int)
+	quit := make(chan bool)
+	go shower(ch, quit)
+	for i := 0; i < 10; i++ {
+		ch <- i
+	}
+	quit <- false
+}
+
+func shower(c chan int, q chan bool){
+	for {
+		select {
+			case j := <- c:
+			   fmt.Printf("%d\n", j)
+			case <- q:
+			   break 
+		}	
+		
+	}
+}
